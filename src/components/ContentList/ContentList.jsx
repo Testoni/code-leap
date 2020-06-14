635eabf527cf64897f70887d557f759ed504b304
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-13 22:18:19 -0300

    content list

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import * as dateUtils from '../../utils/dateUtils'
 import { remove } from '../../actions/ContentActions'
 import './ContentList.css'
 
@@ -9,16 +10,19 @@ const ContentList = props => {
     const renderList = () => {
         const list = props.list || []
         return list.map(content => (
-            <div className='panel-list'>
-                <header>
-                    <span>{content.id} - {content.username} - {content.created_datetime} - {content.title} - {content.content}</span>
-                </header>
+            <div key={content.id}>
+                <div className="panel-heading">{content.title}</div>
+                <div className="panel-body">
+                    <span className="label-span">{content.username}</span>
+                    <span className="label-span date-time">{dateUtils.formatDate(content.created_datetime)}</span>
+                    <p className="label-text">{content.content}</p>
+                </div>
             </div>
         ))
     }
 
     return (
-        <div>
+        <div className="panel">
             {renderList()}
         </div>
     )
